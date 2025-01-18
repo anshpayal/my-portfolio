cb7e9bf831ef10e7142f2d16aa116d2c5e0ab3fa
Ansh Payal <anshpayal754@gmail.com>
2025-01-19 01:30:34 +0530

    feat: moved hard coded data into constant.ts

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,38 +1,34 @@
 import Image from "next/image";
-import MyProfilePhoto from "../../public/profile-photo.jpeg";
 import { IoLogoLinkedin } from "react-icons/io5";
 import { FaSquareXTwitter, FaSquareGithub } from "react-icons/fa6";
+import { about } from "@/data/constant";
 
 export default function About() {
+  const { profilePhoto, heading, description, socialLinks } = about;
   return (
     <section className="flex flex-col-reverse md:flex-row md:justify-between md:items-center md:my-16">
       <div className="md:w-8/12">
         <h1 className="text-4xl font-bold pb-1 text-zinc-700 dark:text-white">
-          Ansh Payal
+          {heading}
         </h1>
-        <h2 className="font-light text-zinc-600 dark:text-zinc-400">
-          Frontend Developer with 2+ years of experience specializing in{" "}
-          <b className="text-zinc-800 dark:text-zinc-300">
-            {" "}
-            Next.js, React, Tailwind CSS, API integrations, Web performance
-            optimization
-          </b>
-          , and more. Delivering high-quality, efficient web applications.
-        </h2>
+        <h2
+          className="font-light text-zinc-600 dark:text-zinc-400"
+          dangerouslySetInnerHTML={{ __html: description }}
+        />
         <div className="flex space-x-3 mt-4">
-          <a href="https://linkedin.com/anshpayal" target="_blank">
+          <a href={socialLinks.linkedin} target="_blank">
             <IoLogoLinkedin size={24} className="bg-zinc-900" />
           </a>
-          <a href="https://github.com/anshpayal" target="_blank">
+          <a href={socialLinks.github} target="_blank">
             <FaSquareGithub size={24} className="bg-zinc-900" />
           </a>
-          <a href="https://twitter.com/anshpayal18" target="_blank">
+          <a href={socialLinks.twitter} target="_blank">
             <FaSquareXTwitter size={24} className="bg-zinc-900" />
           </a>
         </div>
       </div>
       <Image
-        src={MyProfilePhoto}
+        src={profilePhoto}
         alt="Ansh Payal"
         className="w-32 h-32 rounded-xl mb-6 md:mb-0"
       />
--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,107 +1,18 @@
 "use client";
 
 import { useState } from "react";
-import Image, { StaticImageData } from "next/image";
+import Image from "next/image";
 import Link from "next/link";
 import Modal from "./Modal";
-
-import CrowdsecImage from "../../public/crowdsec-project.png";
-import KrakendImage from "../../public/krakend-project.png";
-import SnipurlImage from "../../public/snipurl-project.png";
-import SharkdomImage from "../../public/sharkdom-project.png";
-
+import { ProjectType, projects } from "@/data/constant";
 import { FaLink } from "react-icons/fa6";
 
-type Project = {
-  title: string;
-  description: string;
-  techStack: string[];
-  image: StaticImageData;
-  url: string;
-  tag: string;
-  contribution?: string[];
-};
-
 export default function Project() {
-  const [selectedProject, setSelectedProject] = useState<Project | null>(null);
+  const [selectedProject, setSelectedProject] = useState<ProjectType | null>(
+    null
+  );
 
-  const projects: Project[] = [
-    {
-      title: "Crowdsec",
-      description:
-        "CrowdSec provides open source solution for detecting and blocking malicious IPs, safeguarding both infrastructure and application security.",
-      techStack: [
-        "Next.js",
-        "React",
-        "Javascript",
-        "Tailwind",
-        "GraphQL",
-        "Headless CMS",
-      ],
-      image: CrowdsecImage,
-      url: "https://www.crowdsec.net/",
-      tag: "Full-time",
-      contribution: [
-        "Developed the CrowdSec website using Next.js and TypeScript with WordPress as a headless CMS via GraphQL.",
-        "Built a scalable dynamic component system supporting 18+ layouts, reducing clients page development time by 80%.",
-        "Integrated Algolia for advanced blog search and filtering.",
-        "Implemented IP verification with CrowdSec’s public API, including blocklist checks, removal requests, and rate-limiting for reliability.",
-      ],
-    },
-    {
-      title: "KrakenD",
-      description:
-        "KrakenD is an open-source API Gateway that helps you effortlessly adopt microservices and secure communications.",
-      techStack: [
-        "Next.js",
-        "React",
-        "JavaScript",
-        "Tailwind",
-        "Websocket",
-        "MDX component",
-      ],
-      image: KrakendImage,
-      url: "https://www.krakend.io/enterprise/",
-      tag: "Full-time",
-      contribution: [
-        "Built the KrakenD Playground website with Next.js, providing an interactive demo platform for exploring API gateway features and testing endpoints.",
-        "Developed a WebSocket-based real-time chat system with room-based functionality and connection management for API endpoint use cases.",
-        "Integrated MDX library for dynamic content updates, enabling client to easily modify API endpoint descriptions.",
-      ],
-    },
-    {
-      title: "Sharkdom",
-      description:
-        "Discover and connect with the right partners to boost revenue, enhance offerings, and unlock new opportunities—all on one seamless platform.",
-      techStack: [
-        "Next.js",
-        "React",
-        "Javascript",
-        "Tailwind CSS",
-        "Redux Toolkit",
-        "Vercel Deployment",
-      ],
-      image: SharkdomImage,
-      url: "https://www.sharkdom.com/",
-      tag: "Part-time",
-      contribution: [
-        "Integrated a RESTful API to enable dynamic pricing based on INR and USD.",
-        "Implemented a new user login and registration flow and done Deployment of the application using Vercel and the Vercel CLI.",
-        "Resolved responsiveness issues and added animations to enhance component interactions.",
-      ],
-    },
-    {
-      title: "SnipUrl",
-      description:
-        "SnipURL is a URL shortening tool built with ReactJS and Supabase. Easily shorten long URLs and track their analytics for better link management.",
-      techStack: ["React", "Javascript", "Tailwind CSS", "Supabase", "Shadcn"],
-      image: SnipurlImage,
-      url: "https://snipurl.co/",
-      tag: "Personal Project",
-    },
-  ];
-
-  const openModal = (project: Project) => {
+  const openModal = (project: ProjectType) => {
     setSelectedProject(project);
   };
 
